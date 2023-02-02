d1fe40e66dad0f2f9dad93c68237b1331fa66089
Reidwilliamson13 <86805417+Reidwilliamson13@users.noreply.github.com>
2023-02-01 20:24:46 -0600

    footer

--- dctortas-cra/src/components/Footer.js
+++ dctortas-cra/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(props) {
   return (
     <footer>
-      <div></div>
+      <div>&copy; 2023 Dream Center Tortas a ministry of Church Eleven-32</div>
     </footer>
   );
 }
