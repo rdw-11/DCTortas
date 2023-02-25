298ae9ff8486d0073c7552544ac7c67780646a24
Reid Williamson <86805417+rdw-11@users.noreply.github.com>
2023-02-25 15:44:02 -0500

    routes remake

--- dctortas-cra/src/App.js
+++ dctortas-cra/src/App.js
@@ -3,17 +3,25 @@ import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import PageLayout from "./layout/PageLayout";
 import ForgotPw from "./pages/ForgotPw";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<PageLayout />}>
-          <Route index element={<ForgotPw />} />
-        </Route>
-      </Routes>
-    </div>
+    <>
+      <Header />
+      <div className="App">
+        <Routes>
+          <Route path="/forgotpw" element={<ForgotPw />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+// <Routes>
+//   <Route path="/" element={<PageLayout />}>
+//     <Route index element={<ForgotPw />} />
+//   </Route>
+// </Routes>;
